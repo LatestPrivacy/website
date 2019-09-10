5640684e8270fc9fa136779fcd9418fa100aae00
NIZAMIL PUTRA <nizamil.putra@gmail.com>
2019-09-10 16:21:40 +0700

    Plato hover interaction menu!  🎉

--- src/components/AboutBanner.js
+++ src/components/AboutBanner.js
@@ -6,11 +6,33 @@ import BannerStyle from './AboutBanner.module.scss'
 import winnerImg from '../assets/images/winner.jpg'
 
 class AboutBanner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { width: 0, height: 0 };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+    
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+    
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
     render() {
         return (
             <div className={ `${BannerStyle.container} container` }>
                 <h1 className={BannerStyle.Title}>We are a small team that packs a punch.</h1>
-                <Parallax className={BannerStyle.parallaxImg} y={[0, -20]} tagOuter="figure">
+                <Parallax className={BannerStyle.parallaxImg} y={[0, -20]} tagOuter="figure"
+                disabled={
+                    window.innerWidth < 768 ? true : false
+                }
+                >
                     <img src={winnerImg} alt="We are a small team that packs a punch" className={BannerStyle.winnerImg}/>
                 </Parallax>
                 <Parallax className={BannerStyle.parallaxTxt} y={[30, -60]} tagOuter="figure">
--- src/components/LinkMenu.js
+++ src/components/LinkMenu.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-import LinkMenuStyle from "./LinkMenu.module.scss"
-
 class LinkMenu extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {hover: true};
-    
-        // This binding is necessary to make `this` work in the callback
-        this.toggleHover = this.toggleHover.bind(this);
-    }
-    toggleHover() {
-        this.setState(state => ({
-            hover: !state.hover
-        }));
-        console.log(this.state.hover);
-    }
     render(){
         return(
-            <li 
-            onMouseEnter={this.toggleHover}
-            onMouseLeave={this.toggleHover} >
+            <li>
                 <Link to={this.props.url}>{this.props.text}</Link>
-                <div 
-                    className={`
-                        ${LinkMenuStyle.menuBorder}
-                        ${this.state.hover ? LinkMenuStyle.menuBorderHide : LinkMenuStyle.menuBorderShow}
-                    `}
-                >
-                    <span></span>
-                </div>
             </li>
         )
     }
