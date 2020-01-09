aa0d3038eadf4d4959f7302babc54d1668e28c1f
NIZAMIL PUTRA <nizamil.putra@gmail.com>
2020-01-10 05:38:56 +0700

    Add eye video background on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,18 @@ import HomeMission from '../components/HomeMission'
 import HomeVideo from '../components/HomeVideo'
 import HomeDonate from '../components/HomeDonate'
 
+import video from '../assets/eye.mp4'
+
 import Style from './Home.module.scss'
 
 class Home extends Component {
     render() {
         return (
             <div className={Style.wrapper}>
+                <video class={Style.videoBG} autoPlay muted loop>
+                    <source src={video} type="video/mp4" />
+                </video>
+                
                 <HomeBanner />
                 <HomeNews />
                 <HomeOrganisation />
