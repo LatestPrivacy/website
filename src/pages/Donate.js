582ec965f483b6a9713d66d96777148fdb17b92c
Luke <luke@safing.io>
2020-04-21 14:34:22 +0200

    Update donate meta tags

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import {Helmet} from 'react-helmet'
 
 class Donate extends Component {
     render() {
         return (
-            <div className="container">
-                This is Donate
-            </div>
+            <>
+                <Helmet>
+                    <title>Latest Privacy - Support Us</title>
+                    <meta name="description" content="Help us spread awareness about people's rights to privacy." />
+                    <meta name="keywords" content="latest privacy, videos, publish, technology, security, privacy, surveillance, human rights, encryption, law, investigations, research, internet, united kingdom, GDPR, data protection, artificial intelligence" />
+                </Helmet>
+                <div className="container">
+                    This is Donate
+                </div>
+            </>
         );
     }
 }
