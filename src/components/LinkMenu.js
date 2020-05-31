800ef0643f977d06e73a55bb1a3f034bc27cfccb
NIZAMIL PUTRA <nizamil.putra@gmail.com>
2020-05-31 21:04:00 +0700

    Fixing navigation issue #11

--- src/components/LinkMenu.js
+++ src/components/LinkMenu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 class LinkMenu extends Component {
     render(){
@@ -7,7 +8,10 @@ class LinkMenu extends Component {
             <li>
                 { 
                     this.props.type === 'hash' ?
-                    <a href={this.props.url}>{this.props.text}</a>
+                    <NavLink
+                        to={`/${this.props.url}`}
+                        activeClassName="selected"
+                        >{this.props.text}</NavLink>
                     : <Link to={this.props.url}>{this.props.text}</Link>
                 }
             </li>
