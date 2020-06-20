7b170ba8fff2bef6b2ef8efdf7906feac016e04a
Luke Seers <16389426+LukeSeers@users.noreply.github.com>
2020-06-20 19:15:22 +0200

    Fix links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ class Footer extends Component {
         return (
             <div className={ `${FooterStyle.container} container` }>
                 <ul className={FooterStyle.top}>
-                    <li><a href="http://#">Terms & Conditions</a></li>
-                    <li><a href="http://#">Sitemap</a></li>
+                    <li><a href="/terms-and-conditions">Terms & Conditions</a></li>
+                    <li><a href="/sitemap.xml">Sitemap</a></li>
                 </ul>
                 <div className={FooterStyle.right}>All Rights Reserved. All trademarks registered trademarks are the property of their respective owners</div>
                 <div className={FooterStyle.powered}>Powered by volunteers & donations</div>
