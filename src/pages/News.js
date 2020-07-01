43a707da37d40104bfee4691026349dd0e22a308
Luke Seers <16389426+LukeSeers@users.noreply.github.com>
2020-07-01 02:25:57 +0200

    Add react-infinite-scroll-component

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react'
 import {Helmet} from 'react-helmet'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const style = {
+    height: 30,
+    border: "1px solid green",
+    margin: 6,
+    padding: 8
+  };
 
 class News extends Component {
+    state = {
+        items: Array.from({ length: 20 })
+      };
+
+    fetchMoreData = () => {
+        // a fake async api call like which sends
+        // 20 more records in 1.5 secs
+        setTimeout(() => {
+          this.setState({
+            items: this.state.items.concat(Array.from({ length: 20 }))
+          });
+        }, 1500);
+      };
+      
     render() {
         return (
             <>
@@ -11,7 +33,18 @@ class News extends Component {
                     <meta name="keywords" content="latest privacy, articles, publish, technology, security, privacy, surveillance, human rights, encryption, law, investigations, research, internet, united kingdom, GDPR, data protection, artificial intelligence" />
                 </Helmet>
                 <div className="container">
-                    This is News
+                <InfiniteScroll
+                dataLength={this.state.items.length}
+                next={this.fetchMoreData}
+                hasMore={true}
+                loader={<h4>Loading...</h4>}
+                >
+                {this.state.items.map((i, index) => (
+                    <div style={style} key={index}>
+                    div - #{index}
+                    </div>
+                ))}
+                </InfiniteScroll>
                 </div>
             </>
         );
