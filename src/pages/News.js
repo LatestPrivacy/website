f55c6b905d64ed511458458fa4f963f25c27dc0f
Johnathon Weaver <weaver123_johnathon@hotmail.com>
2020-09-30 03:27:44 +0800

    Random update

--- src/pages/News.js
+++ src/pages/News.js
@@ -21,10 +21,12 @@ class News extends Component {
 		data: {},
 		length: 0,
 		loading: false,
-		more: true
+		more: true,
+		value: '',
+		result: []
 	}
 
-	async loadArticles() {
+	loadArticles = async () => {
 		const { data, length, loading, more } = this.state;
 		if ( loading || !more ) { return; };
 
@@ -32,12 +34,7 @@ class News extends Component {
 
 		const response = await axios.get( `/api/articles?sort=${dateStr}&description=true` );
 
-		if ( response.data.length < 1 ) {
-
-			this.setState( { more: false } );
-
-		} else {
-
+		if ( response.data.length > 0 ) {
 			this.setState( {
 				data: {
 					...data,
@@ -48,7 +45,28 @@ class News extends Component {
 
 			date.setMonth( date.getMonth() - 1 );
 			dateStr = moment( date ).format( 'MM-YYYY' );
-			
+		} else {
+			this.setState( { more: false } );
+		};
+
+		this.setState( { loading: false } );
+	}
+
+	handleTextChange = ( event ) => {
+		this.setState( { value: event.target.value } );
+	}
+
+	handleSubmit = async ( event ) => {
+		event.preventDefault();
+
+		const { data, value } = this.state;
+
+		this.setState( { loading: true } );
+
+		const response = await axios.get( `/api/articles?search=${value}&description=true&limit=100` );
+
+		if ( response.data.length > 0 ) {
+			this.setState( { result: response.data } );
 		};
 
 		this.setState( { loading: false } );
@@ -61,7 +79,7 @@ class News extends Component {
 	}
 
 	render() {
-		const { data, length, loading, more } = this.state;
+		const { data, length, loading, more, value, result } = this.state;
 
 		return (
 			<>
@@ -70,91 +88,147 @@ class News extends Component {
 					<meta name="description" content="The latest articles that we publish." />
 					<meta name="keywords" content="latest privacy, articles, publish, technology, security, privacy, surveillance, human rights, encryption, law, investigations, research, internet, united kingdom, GDPR, data protection, artificial intelligence" />
 				</Helmet>
+
+				<div>
+					<form onSubmit={this.handleSubmit}>
+						<input
+							className="react-search-field-input"
+							style={{}}
+							onChange={this.handleTextChange}
+							placeholder={'search'}
+							type="text"
+							value={value}
+						/>
+					</form>
+				</div>
+
 				<div className={`${Style.container} container`}>
 					<InViewMonitor classNameInView="animated-in">
-						<InfiniteScroll
-							dataLength={length}
-							next={async () => {await this.loadArticles();}}
-							hasMore={more}
-							scrollThreshold={0.75}
-							loader={
-								<div className={Style.loading}>
-									<SyncLoader
-										size={8}
-										color={'#656565'}
-										loading={loading}
-									/>
-								</div>
-							}
-							endMessage={
-								<div className={Style.loading}>
-									<b>Yay! You have seen everything, come back later for more articles.</b>
-								</div>
-							}
-						>
-							<div className={Style.newsWrapper}>
-								{
-									Object.entries(data).map(([key, item], index) => {
-
-										return (
-
-											<>
-
-												{(index > 0) &&
-													<div className={Style.advert}>
-														<h3>{key}</h3>
-													</div>
+						{((value.length > 0 && loading) || result.length > 0) ?
+							(
+								<>
+									{(!loading) ?
+										(
+											<div className={Style.newsWrapper}>
+												{
+													result.map((article, i) => (
+														<>
+															<NewsItem
+																author={article.publisher}
+																date={article.published_on}
+																/*timetoread={article.read_time}*/  /*NewsItem.js: Line 35*/ /*NewsDetail.js: Line 72*/
+																slug={article.slug}
+																/*delay={ 0.6 + (i * 0.3) }*/
+															>
+																<>
+																	<h2>
+																		{article.title}
+																	</h2>
+																	<p>
+																		{article.description}
+																	</p>
+																</>
+															</NewsItem>
+														</>
+													))
 												}
-
-												<>
-													{
-														item.map((article, i) => (
-															<>
-																<NewsItem
-																	author={article.publisher}
-																	date={article.published_on}
-																	/*timetoread={article.read_time}*/  /*NewsItem.js: Line 35*/ /*NewsDetail.js: Line 72*/
-																	slug={article.slug}
-																	/*delay={ 0.6 + (i * 0.3) }*/
-																>
-																	<>
-																		<h2>
-																			{article.title}
-																		</h2>
-																		<p>
-																			{article.description}
-																		</p>
-																	</>
-																</NewsItem>
-																{!((i+1) % (artLimit*6)) &&
-																	<a href="/#donate" className={Style.advert}>
-																		<h3>
-																			Please support us
-																		</h3>
-																		<div>
-																			<p>
-																				By supporting, it enables us to carry on 
-																				spreading awareness to a bigger audience 
-																				about our right to privacy with tools 
-																				that make it easier to follow the privacy 
-																				world.
-																			</p>
-																		</div>
-																	</a>
-																}
-															</>
-														))
-
-													}
-												</>
-
-											</>
+											</div>
+										) : (
+											<div className={Style.loading}>
+												<SyncLoader
+													size={8}
+													color={'#656565'}
+													loading={loading}
+												/>
+											</div>
 										)
-
-									})
-								}
-							</div>
-						</InfiniteScroll>
+									}
+								</>
+							) : (
+								<InfiniteScroll
+									dataLength={length}
+									next={this.loadArticles}
+									hasMore={more}
+									scrollThreshold={0.6}
+									loader={
+										<div className={Style.loading}>
+											<SyncLoader
+												size={8}
+												color={'#656565'}
+												loading={loading}
+											/>
+										</div>
+									}
+									endMessage={
+										<div className={Style.loading}>
+											<b>Yay! You have seen everything, come back later for more articles.</b>
+										</div>
+									}
+								>
+									<div className={Style.newsWrapper}>
+										{
+											Object.entries(data).map(([key, item], index) => {
+												return (
+													<>
+														{(index > 0) ?
+															(
+																<div className={Style.advert}>
+																	<h3>{key}</h3>
+																</div>
+															) : (
+																<div>
+																	<h3>First Block Here</h3>
+																</div>
+															)
+														}
+														<>
+															{
+																item.map((article, i) => (
+																	<>
+																		<NewsItem
+																			author={article.publisher}
+																			date={article.published_on}
+																			/*timetoread={article.read_time}*/  /*NewsItem.js: Line 35*/ /*NewsDetail.js: Line 72*/
+																			slug={article.slug}
+																			/*delay={ 0.6 + (i * 0.3) }*/
+																		>
+																			<>
+																				<h2>
+																					{article.title}
+																				</h2>
+																				<p>
+																					{article.description}
+																				</p>
+																			</>
+																		</NewsItem>
+																		{!((i+1) % (artLimit*6)) &&
+																			<a href="/#donate" className={Style.advert}>
+																				<h3>
+																					Please support us
+																				</h3>
+																				<div>
+																					<p>
+																						By supporting, it enables us to carry on 
+																						spreading awareness to a bigger audience 
+																						about our right to privacy with tools 
+																						that make it easier to follow the privacy 
+																						world.
+																					</p>
+																				</div>
+																			</a>
+																		}
+																	</>
+																))
+															}
+														</>
+													</>
+												)
+											})
+										}
+									</div>
+								</InfiniteScroll>
+							)
+						}
 					</InViewMonitor>
 				</div>
 			</>
